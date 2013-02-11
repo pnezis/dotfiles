32f22932fbd0818abc4a7d8682d71148414ffa2a
Panagiotis Nezis <pnezis@gmail.com>
2013-02-11 22:29:11 +0200

    install zsh configuration

--- install.sh
+++ install.sh
@@ -27,13 +27,39 @@ remove_file_dir ()
 }
 
 
+install_oh_my_zsh ()
+{
+    if [ -d "$HOME/.oh-my-zsh" ]; then
+        echo "Oh-my-zsh is already installed"
+    else
+        echo "Installing Oh-my-zsh"
+        git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
+    fi
+}
+
+switch_to_zsh ()
+{
+    # Check if current shell is already ZSH
+    if [[ "$SHELL" == *"zsh"* ]]; then
+        echo "Zsh is already the default shell"
+    else
+        # Change the shell
+        chsh -s `which zsh`
+    fi
+}
+
+install_oh_my_zsh
+switch_to_zsh
+
 echo "Removing existing configuration files..."
 remove_file_dir $HOME/.vimrc
 remove_file_dir $HOME/.vim
+remove_file_dir $HOME/.zshrc
 
 echo "Creating symbolic links..."
 ln -s $DIR/vimrc $HOME/.vimrc
 ln -s $DIR/vim $HOME/.vim
+ln -s $DIR/zshrc $HOME/.zshrc
 
 echo "Updating repository..."
 git pull && git submodule init && git submodule update && git submodule status
