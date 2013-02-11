172520d341c23dbd672239e6f53c509faa68fe42
Panagiotis Nezis <pnezis@gmail.com>
2013-02-12 00:10:46 +0200

    Copy terminal profile

--- install.sh
+++ install.sh
@@ -30,9 +30,9 @@ remove_file_dir ()
 install_oh_my_zsh ()
 {
     if [ -d "$HOME/.oh-my-zsh" ]; then
-        echo "Oh-my-zsh is already installed"
+        echo "Oh-my-zsh is already installed!"
     else
-        echo "Installing Oh-my-zsh"
+        echo "Installing Oh-my-zsh..."
         git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
     fi
 }
@@ -41,28 +41,43 @@ switch_to_zsh ()
 {
     # Check if current shell is already ZSH
     if [[ "$SHELL" == *"zsh"* ]]; then
-        echo "Zsh is already the default shell"
+        echo "Zsh is already the default shell!"
     else
         # Change the shell
         chsh -s `which zsh`
+        echo "Changed the shell to zsh!"
     fi
 }
 
-install_oh_my_zsh
-switch_to_zsh
+copy_terminal_profile()
+{
+    if [ -d "$HOME/.gconf/apps/gnome-terminal/profiles/Default/" ]; then
+        echo "Copying terminal profile..."
+        cp "$DIR/gconf.xml" "$HOME/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml"
+    else
+        echo "Could not copy temrinal profile!"
+    fi
+}
 
+echo
+install_oh_my_zsh
+echo
+switch_to_zsh
+echo
+copy_terminal_profile
+echo
 echo "Removing existing configuration files..."
 remove_file_dir $HOME/.vimrc
 remove_file_dir $HOME/.vim
 remove_file_dir $HOME/.zshrc
-
+echo
 echo "Creating symbolic links..."
 ln -s $DIR/vimrc $HOME/.vimrc
 ln -s $DIR/vim $HOME/.vim
 ln -s $DIR/zshrc $HOME/.zshrc
-
+echo
 echo "Updating repository..."
 git pull && git submodule init && git submodule update && git submodule status
-
 echo
-echo "Start VIM and run :BundleInstall"
+echo
+echo "Successfully installed Dot Files. Start VIM and run :BundleInstall"
