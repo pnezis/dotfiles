675a72c56fdf4d200efeb14081bf73af1d405243
Panagiotis Nezis <pnezis@gmail.com>
2013-02-16 19:18:57 +0200

    Improved install script

--- install.sh
+++ install.sh
@@ -17,22 +17,44 @@ remove_file_dir ()
 {
     if [ -L "$1" ]; then
         rm "$1"
+        success "removed $1"
     else
         if [ -f "$1" ]; then
             rm "$1"
+            success "removed $1"
         elif [ -d "$1" ]; then
             rm -rf "$1"
+            success "removed $1"
         fi
     fi
 }
 
+# Functions borrowed from:
+# https://github.com/holman/dotfiles/blob/master/script/bootstrap
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+link_files () {
+  ln -s $1 $2
+  success "linked $1 to $2"
+}
 
 install_oh_my_zsh ()
 {
     if [ -d "$HOME/.oh-my-zsh" ]; then
-        echo "Oh-my-zsh is already installed!"
+        success "Oh-my-zsh is already installed!"
     else
-        echo "Installing Oh-my-zsh..."
         git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
     fi
 }
@@ -41,21 +63,24 @@ switch_to_zsh ()
 {
     # Check if current shell is already ZSH
     if [[ "$SHELL" == *"zsh"* ]]; then
-        echo "Zsh is already the default shell!"
+        success "Zsh is already the default shell!"
     else
         # Change the shell
         chsh -s `which zsh`
-        echo "Changed the shell to zsh!"
+        success "Changed the shell to zsh!"
     fi
 }
 
+echo "Installing Oh-my-zsh..."
 install_oh_my_zsh
-echo
+
+echo "Making zsh default shell..."
 switch_to_zsh
-echo
-echo "Copying zsh theme"
+
+echo "Updating zsh theme..."
 cp "$DIR/zsh/pnezis.zsh-theme" "$HOME/.oh-my-zsh/themes/pnezis.zsh-theme"
-echo
+success "Updated zsh theme"
+
 echo "Removing existing configuration files..."
 remove_file_dir $HOME/.vimrc
 remove_file_dir $HOME/.vim
@@ -63,20 +88,26 @@ remove_file_dir $HOME/.zshrc
 remove_file_dir $HOME/.dircolors
 remove_file_dir $HOME/.tmux
 remove_file_dir $HOME/.tmux.conf
-echo
+
 echo "Creating symbolic links..."
-ln -s $DIR/vimrc $HOME/.vimrc
-ln -s $DIR/vim $HOME/.vim
-ln -s $DIR/zshrc $HOME/.zshrc
-ln -s $DIR/dircolors.256dark $HOME/.dircolors
-ln -s $DIR/tmux $HOME/.tmux
-ln -s $DIR/tmux.conf $HOME/.tmux.conf
-echo
+link_files $DIR/vim $HOME/.vim
+link_files $DIR/vim/vimrc $HOME/.vimrc
+link_files $DIR/zsh/zshrc $HOME/.zshrc
+link_files $DIR/misc/dircolors.256dark $HOME/.dircolors
+link_files $DIR/tmux $HOME/.tmux
+link_files $DIR/tmux/tmux.conf $HOME/.tmux.conf
+
 echo "Updating repository..."
 git pull && git submodule init && git submodule update && git submodule status
-echo
-echo "Updating gnome terminala profile"
-python $DIR/molokai-terminal.py
+success "Updated repository and submodules"
+
+echo "Updating gnome terminal profile..."
+python $DIR/setup-scripts/molokai-terminal.py
+success "Updated terminal profile"
+
 echo "Setting git colors"
-$DIR/gitcolors.sh
-echo "Successfully installed Dot Files. Start VIM and run :BundleInstall"
+$DIR/setup-scripts/gitcolors.sh
+success "Applied global git colors config"
+
+echo
+echo "Successfully installed Dot Files. Restart terminal, start VIM and run :BundleInstall"
